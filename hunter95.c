3b5b104348bf4109d2862f39d5d18893a0769cfa
konoha-kido <50362443+konoha-kido@users.noreply.github.com>
2019-06-11 13:39:24 +0530

    Update hunter95.c

--- hunter95.c
+++ hunter95.c
@@ -5,6 +5,11 @@ int main()
 {
     int i=0,n;
     scanf("%d",&n);
+
+if(n<=2){
+printf("0");
+return 0;}
+
 int arr[max],prime[max];
 for(i=0;i<=n;i++)
 {
@@ -13,7 +18,7 @@ for(i=0;i<=n;i++)
 
 arr[0]=arr[1]=0;
 printf("2");
-for(i=3;i<=n;i++)
+for(i=3;i<n;i++)
 {
 
     if(arr[i]==1)
