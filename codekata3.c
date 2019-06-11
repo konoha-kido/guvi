f2681d756b23d4f484e53ff329f9336fc9834161
konoha-kido <50362443+konoha-kido@users.noreply.github.com>
2019-06-11 09:34:00 +0530

    Update codekata3.c

--- codekata3.c
+++ codekata3.c
@@ -3,6 +3,11 @@ int main()
 {
 char a;
 scanf("%c",&a);
+ if(a<97)a+=32;
+ if(a<97||a>122)
+ {printf("invalid");
+  return 0;}
+ 
 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')printf("Vowel");
 else printf("Consonant");
 }
