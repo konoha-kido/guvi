e87164712be6920998f258ba8873f97748f48d6d
konoha-kido <50362443+konoha-kido@users.noreply.github.com>
2019-06-11 13:37:09 +0530

    Create hunter95.c

---
+++ hunter95.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#define max 1001
+int main()
+{
+    int i=0,n;
+    scanf("%d",&n);
+int arr[max],prime[max];
+for(i=0;i<=n;i++)
+{
+    arr[i]=1;
+}
+
+arr[0]=arr[1]=0;
+printf("2");
+for(i=3;i<=n;i++)
+{
+
+    if(arr[i]==1)
+    {
+        printf(" %d",i);
+    
+        for(int j=i*i;j<max;j+=i)
+        {
+            arr[j]=0;
+        }
+}
+}
+}
