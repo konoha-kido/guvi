571a88c5417fc22efd91e2a512fbc5a79d1ff3c7
konoha-kido <50362443+konoha-kido@users.noreply.github.com>
2019-06-11 10:18:45 +0530

    Create codekata12.c

---
+++ codekata12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,d,sum=0;
+scanf("%d",&a);
+int safe=a;
+while(a)
+{
+    d=a%10;
+    sum=sum*10+d;
+    a/=10;
+}
+
+if(sum==safe)
+printf("Palindrome");
+}
