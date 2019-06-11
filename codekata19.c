0104972447d0989aa4f449249eb0eced69fe8988
konoha-kido <50362443+konoha-kido@users.noreply.github.com>
2019-06-11 10:26:46 +0530

    Update codekata19.c

--- codekata19.c
+++ codekata19.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,fact=1;
-scanf("%d",&a);
+int var,fact=1;
+scanf("%d",&var);
 fact=1;
-if(a==0){
-printf("1");return 0;}
+if(a==0)
+{
+printf("1");
+    return 0;
+}
 while(a>=1)
 {
     fact*=a;
